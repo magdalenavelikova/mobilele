d148f44c947b0baa0ffce2f4121219c8314c3f7d
MLVelikova <magibox@yahoo.com>
2023-07-18 13:58:59 +0300

    lang with Locale Change nterceptor

---
+++ src/main/java/bg/softuni/mobilele/config/WebConfig.java
@@ -0,0 +1,20 @@
+package bg.softuni.mobilele.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LocaleChangeInterceptor lci;
+
+    public WebConfig(LocaleChangeInterceptor lci) {
+        this.lci = lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+     registry.addInterceptor(lci);
+    }
+}
